ac382f02fc47666b6c08b126d99c890f2b93c5d6
Ritvik <youritvik02@gmail.com>
2024-04-20 14:18:36 -0400

    add DatabaseHelpers class (not finished)

---
+++ AB_game/CIS3433/DatabaseHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CIS3433
+{
+    public class DatabaseHelpers
+    {
+        public static void insertCodemakerSession(string connString, string code, string codemakerName)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand("AddCodemakerSession", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Parameters.AddWithValue("@FIRST_NAME", SqlDbType.NVarChar).Value = firstName;
+            //cmd.Parameters.AddWithValue("@LAST_NAME", SqlDbType.NVarChar).Value = lastName;
+            //cmd.Parameters.AddWithValue("@AGE", SqlDbType.Int).Value = age;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
